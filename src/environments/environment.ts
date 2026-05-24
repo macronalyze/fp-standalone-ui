73dbdf74fcb6f97cb387775eea4d73ca9deffd43
naridnevahgar <naridnevahgar@gmail.com>
2026-05-25 01:21:32 +0900

    env fix

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   googleClientId: '435159057812-u6glnejvlms88ugsht08oa70nv4nigv3.apps.googleusercontent.com',
-  apiUrl: 'https://fp-render-eu.onrender.com/fp/api/v1',
+  apiUrl: 'https://fp-backend-eu.onrender.com/fp/api/v1',
 };
